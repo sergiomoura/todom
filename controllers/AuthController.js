3bc11c9032445bbd561bf243969f35759bce587d
Sérgio Moura <smouracalmon@gmail.com>
2020-11-24 16:15:08 -0300

    Trata o login e, em caso de sucesso, dispara req para carregar tarefas

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,37 @@
+const { Usuario } = require('../models'); 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 module.exports = {
-    login: (req, res) =>{
-        console.log(req.body);
-        return res.json(req.body);
+    login: async (req, res) =>{
+
+        // Capturar as informações da requisição
+        const {email, senha} = req.body;
+
+        // Carregar o usuário que seja dono do email enviado
+        const user = await Usuario.findOne({ where:{ email }});
+
+        // Tratando o caso de usuário inexistente
+        if(!user){
+            return res.status(401).json({error: "Usuário/Senha inválidos"});
+        }
+
+        // Validar o usuário
+        const loginOk = await bcrypt.compare(senha, user.senha);
+
+        // Tratando o caso da validação da senha falhar
+        if(!loginOk){
+            return res.status(401).json({error: "Usuário/Senha inválidos"});
+        }
+
+        // Arrancando a senha do obj user
+        user.senha = undefined;
+
+        // Gerar o token (jwt - biblioteca: jsonwebtoken)
+        const token = jwt.sign({user}, "palavrasecreta");
+
+        // Enviar o token
+        return res.json({user, token});
+
     }
 }
--- public/js/login.js
+++ public/js/login.js
@@ -23,13 +23,56 @@ btLogar.addEventListener('click', onBtLogarClick);
 
 // Criar uma função login(email,senha) que deve disparar uma requisição post
 // para a rota /login
-const login = (email,senha) => {
+const login = async (email,senha) => {
 
-    fetch('/login', {
+    // Capturei a resposta do servidor
+    let response = await fetch('/login', {
         method: 'POST',
         body: JSON.stringify({ email: email ,senha: senha }),
         headers: { 'Content-Type': 'application/json' },
     });
+    
+    // Interpretar essa resposta como json
+    let dados = await response.json();
+
+    // Verificando se o login falhou
+    if(dados.error){
+        exibirErroDeLogin();
+        return;
+    }
+
+
+    // Guardar o token no sessionStorage
+    sessionStorage.setItem("token", dados.token);
+
+    // Guardar o user no sessionStorage
+    sessionStorage.setItem("user", JSON.stringify(dados.user));
+
+    // Esconder o form de login e motrar a lista de tarefas
+    toggleLogin();
+
+    // Carregar as tarefas do servidor
+    carregarTarefas();
 
 }
 
+const toggleLogin = () => {
+    document.getElementById("login").style.display = "none";
+    document.querySelector("main").style.display = "block";
+}
+
+const exibirErroDeLogin = () => {
+    console.error("Falha no login");
+}
+
+const carregarTarefas = () => {
+
+    // Disparar a requisição get para /tarefas
+    fetch('/tarefas', {
+        method: "GET",
+        headers:{
+            authorization: `bearer ${sessionStorage.getItem('token')}`
+        }
+    });
+
+}
